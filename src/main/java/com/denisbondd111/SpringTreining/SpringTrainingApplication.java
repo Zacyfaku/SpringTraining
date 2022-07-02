c1e9d0cfba7d4339256c057b15c7f835808be482
Zacyfaku <denisbondd111@gmail.com>
2022-07-02 17:54:09 +0300

    Init commit

---
+++ src/main/java/com/denisbondd111/SpringTreining/SpringTrainingApplication.java
@@ -0,0 +1,11 @@
+package com.denisbondd111.SpringTreining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTrainingApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTrainingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/denisbondd111/SpringTreining/SpringTreiningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denisbondd111.SpringTreining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTreiningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
